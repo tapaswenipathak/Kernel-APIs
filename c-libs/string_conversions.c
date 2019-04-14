441736c827ec5d058d3e1aa90c2208d1b9e6848d
Tapasweni Pathak <tapasweni.pathak@ril.com>
2019-04-14 14:40:18 +0530

    Adding few implementations

--- c-libs/string_conversions.c
+++ c-libs/string_conversions.c
@@ -32,6 +32,20 @@ int kstrtouint(cc *s, ui base, ui *res);
 int kstrtoint(cc *s, ui base, int *res);
 int kstrtobool(cc *s, bool *res);
 
+char msg_array[90];
+
+char *vsnprintf_implementation(char *format, ...)
+{
+	va_list arglist;
+	va_start(arglist, format);
+	strcpy(msg_array, "Message Array:");
+	vsnprintf(&msg_array[1], 90, format, arglist);
+	int vscnprintf_val = vscnprintf(res, sizeof(val), fmt, args);
+	printf("%d Number of characters written in res are:", vscnprintf_val);
+	va_end(arglist);
+	return(msg_array);
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
@@ -41,6 +55,46 @@ int main(int argc, char *argv[])
 
 	char *input_str = argv[1];
 
+	ull simple_strtoull_val = simple_strtoull(input_str, null, 0);
+	printf("%llud\n", simple_strtoull_val);
+
+	ul simple_strtoul_val = simple_strtoul(input_str, null, 0);
+	printf("%lud\n", simple_strtoull_val);
+
+	long simple_strtol_val = simple_stroul(input_str, null, 0);
+	printf("%ld\n", simple_stroul_val);
+
+	ll simple_strtoll_val = simple_strtoll(input_str, null, 0);
+	printf("%lld\n", simple_strtoll_val);
+
+	char *msg;
+	msg = vsnprintf_implementation("%s %d %s", "vsnprintf", 100, "times");
+	printf("%s\n", msg);
+
+	int snprintf_val = snprintf(res, len(res), "this is a program");
+	printf("%d\n", snprintf_val);
+
+	int bprintf_val = bprintf(res, len(res), fmt, ...);
+	printf("%d\n", bprintf_val);
+
+	long kstrtol_val = kstrtol(res, 10, &value);
+	printf("%ld", kstrtol_val);
+
+	ul kstrtoul_val = kstrtoul(res, 1, &val);
+	printf("%ul\n", kstrtoul);
+
+	ull kstrtoull_val = kstrtoull_val(res, 1, &val);
+	printf("%ull\n", kstrtoull);
+
+	ll kstrtoll_val = kstrtoll(res, 1, &val);
+	printf("%lld\n", kstrtoll_val);
+
+	ui ksrtouint_val = ksrtouint(res, 1, &val);
+	printf("%ui\n", ksrtouint_val);
+
+	int kstrtoint_val = kstrtoint(res, 1, &val);
+	printf("%d", kstroint_val);
 
 
+	return 0;
 }
